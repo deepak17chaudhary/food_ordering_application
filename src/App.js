cd9b497245cdbadbda121618dfdb3dee4f103518
Deepak Kumar <deepak.kumar@quantela.com>
2023-10-01 00:02:34 +0530

    basic component building)

---
+++ src/App.js
@@ -0,0 +1,16 @@
+
+import Applayout from './Applayout.js';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+      
+    <Applayout />
+       
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Applayout.js
@@ -0,0 +1,12 @@
+import Header from './headercomponent';
+import Body from './bodycontainer';
+const Applayout = () => {
+  return (
+    <div className="applayout">
+      <Header />
+    <Body />
+    </div>
+  );
+};
+
+export default Applayout;
