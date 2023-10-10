4693b7b28c95bfacd9015d5c9bfecac90df17fd1
Deepak Kumar <deepak.kumar@quantela.com>
2023-10-11 01:14:17 +0530

    reset button for restaurent list

--- src/components/BodyComponent.js
+++ src/components/BodyComponent.js
@@ -1,19 +1,29 @@
-
 import RestaurentCard from "./Restautentcard";
 import RestaurentList from "./utils/mockData";
 import { useState } from "react";
 const Body = () => {
-
-const [ListofRestaurent,setListofRestaurent] = useState(RestaurentList);
+  const [ListofRestaurent, setListofRestaurent] = useState(RestaurentList);
 
   return (
     <div className="body">
       <div className="filter">
-        <button className="filter-btn" onClick={()=>{
-          const FilteredList  = ListofRestaurent.filter((res)=> (res.info.rating.rating_text) > 4);
-          setListofRestaurent(FilteredList);
-        }}
-        >Top Rated Restaurant</button></div>
+        <button
+          className="filter-btn"
+          onClick={() => {
+            const FilteredList = ListofRestaurent.filter(
+              (res) => res.info.rating.rating_text > 4
+            );
+            setListofRestaurent(FilteredList);
+          }}
+        >
+          Top Rated Restaurant
+        </button>
+        <button className="filter-btn" onClick={() => {
+         setListofRestaurent(RestaurentList);
+        }}>
+          Reset
+        </button>
+      </div>
       <div className="res-container">
         {ListofRestaurent.map((restaurent) => {
           return (
